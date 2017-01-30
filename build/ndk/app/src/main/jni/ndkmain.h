37708927b395431e6393acaddfe75bf45a487b30
TABATA Keiichi <tabata.k.ichi@gmail.com>
2017-01-30 20:59:17 +0900

    シンボリックリンク修正

--- build/ndk/app/src/main/jni/ndkmain.h
+++ build/ndk/app/src/main/jni/ndkmain.h
@@ -1,10 +1,1 @@
-#ifndef SUIKA_NDKMAIN_H
-#define SUIKA_NDKMAIN_H
-
-#include "types.h"
-#include <jni.h>
-
-extern JNIEnv *jni_env;
-extern jobject main_activity;
-
-#endif
+../../../../../../src/ndkmain.h
