4047d2109b1d812ebc6cbf7afacad052bed63d23
TABATA Keiichi <tabata.k.ichi@gmail.com>
2017-03-07 18:13:21 +0900

    ライブラリのバージョンアップ

--- build/linux64/build-libs.sh
+++ build/linux64/build-libs.sh
@@ -7,15 +7,15 @@ mkdir -p tmp libroot
 
 cd tmp
 
-tar xzf ../../libsrc/zlib-1.2.8.tar.gz
-cd zlib-1.2.8
+tar xzf ../../libsrc/zlib-1.2.11.tar.gz
+cd zlib-1.2.11
 ./configure --prefix=$PREFIX --static
 make
 make install
 cd ..
 
-tar xzf ../../libsrc/libpng-1.6.21.tar.gz
-cd libpng-1.6.21
+tar xzf ../../libsrc/libpng-1.6.28.tar.gz
+cd libpng-1.6.28
 ./configure --prefix=$PREFIX --disable-shared CPPFLAGS=-I$PREFIX/include LDFLAGS=-L$PREFIX/lib
 make
 make install
@@ -35,8 +35,8 @@ make
 make install
 cd ..
 
-tar xzf ../../libsrc/freetype-2.6.3.tar.gz
-cd freetype-2.6.3
+tar xzf ../../libsrc/freetype-2.7.1.tar.gz
+cd freetype-2.7.1
 ./configure --prefix=$PREFIX --disable-shared --with-bzip2 --with-png=no --with-zlib=no --with-harfbuzz=no --with-bzip2=no
 make
 make install
--- build/mingw32/build-libs.sh
+++ build/mingw32/build-libs.sh
@@ -7,16 +7,16 @@ mkdir -p tmp libroot
 
 cd tmp
 
-tar xzf ../../libsrc/zlib-1.2.8.tar.gz
-cd zlib-1.2.8
+tar xzf ../../libsrc/zlib-1.2.11.tar.gz
+cd zlib-1.2.11
 make -f win32/Makefile.gcc
 mkdir -p ../../libroot/include ../../libroot/lib
 cp zlib.h zconf.h ../../libroot/include/
 cp libz.a ../../libroot/lib/
 cd ..
 
-tar xzf ../../libsrc/libpng-1.6.21.tar.gz
-cd libpng-1.6.21
+tar xzf ../../libsrc/libpng-1.6.28.tar.gz
+cd libpng-1.6.28
 ./configure --prefix=$PREFIX --disable-shared CPPFLAGS=-I$PREFIX/include LDFLAGS=-L$PREFIX/lib
 make
 make install
@@ -36,8 +36,8 @@ make
 make install
 cd ..
 
-tar xzf ../../libsrc/freetype-2.6.3.tar.gz
-cd freetype-2.6.3
+tar xzf ../../libsrc/freetype-2.7.1.tar.gz
+cd freetype-2.7.1
 ./configure --disable-shared --with-bzip2 --with-png=no --with-zlib=no --with-harfbuzz=no --with-bzip2=no --prefix=$PREFIX
 make
 make install
